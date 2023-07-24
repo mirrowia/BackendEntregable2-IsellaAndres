a490882cdf1b8bfb4f83b59bff670d9db0e27185
MirrowIA <andresisella@gmail.com>
2023-07-24 07:52:33 -0300

    Verificar funcionamiento

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 const fs = requiere("fs").promises;
 class Contenedor {
   constructor(fileName) {
-    this.filename = fileName;
-    this.id = id;
+    this.fileName = fileName;
   }
 
   async save(Object) {
-    fs.appendFileSync();
+    if (!fs.existSync(this.fileName)) return null;
+    const content = this.getAll();
+    const id = content.length <= 0 ? 1 : content[content.length - 1].id + 1;
+    content.push({ id: id, ...Object });
+    this.saveObjects(content);
   }
 
-  async getById(id) {}
+  async getById(id) {
+    const content = this.getAll();
+    const object = content.find((contentId) => contentId === id);
+    object == undefined
+      ? console.log("No se encontro el objecto con el id: " + id)
+      : object;
+  }
 
-  async getAll() {}
-  deleteById(id) {}
-  deleteAll() {}
+  async getAll() {
+    const content = fs.readFile(this.fileName, "utf-8");
+    return content;
+  }
+
+  deleteById(id) {
+    let content = this.getAll();
+    content = content.filter((obj) => obj.id !== id);
+    this.save(content);
+  }
+
+  deleteAll() {
+    this.save([]);
+  }
 }
