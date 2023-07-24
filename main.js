814126322ee8502df547a22bf8214c1c37399aab
MirrowIA <andresisella@hotmail.com>
2023-07-24 11:18:25 -0300

    Entrega

--- main.js
+++ main.js
@@ -1,37 +1,107 @@
-const fs = requiere("fs").promises;
+const fs = require("fs");
 class Contenedor {
   constructor(fileName) {
     this.fileName = fileName;
   }
 
   async save(Object) {
-    if (!fs.existSync(this.fileName)) return null;
-    const content = this.getAll();
+    const content = await this.getAll();
     const id = content.length <= 0 ? 1 : content[content.length - 1].id + 1;
     content.push({ id: id, ...Object });
-    this.saveObjects(content);
+    try {
+      await fs.promises.writeFile(
+        this.fileName,
+        JSON.stringify(content, null, 2)
+      );
+      return id;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async getById(id) {
-    const content = this.getAll();
-    const object = content.find((contentId) => contentId === id);
-    object == undefined
-      ? console.log("No se encontro el objecto con el id: " + id)
-      : object;
+    try {
+      const content = await this.getAll();
+      const object = content.find((cont) => cont.id === id);
+      return object ? object : null;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async getAll() {
-    const content = fs.readFile(this.fileName, "utf-8");
-    return content;
+    try {
+      const content = await fs.promises.readFile(this.fileName, "utf-8");
+      return content ? JSON.parse(content) : [];
+    } catch (error) {
+      return [];
+    }
   }
 
-  deleteById(id) {
-    let content = this.getAll();
+  async deleteById(id) {
+    let content = await this.getAll();
     content = content.filter((obj) => obj.id !== id);
-    this.save(content);
+    await fs.promises.writeFile(
+      this.fileName,
+      JSON.stringify(content, null, 2)
+    );
   }
 
-  deleteAll() {
-    this.save([]);
+  async deleteAll() {
+    const content = [];
+    await fs.promises.writeFile(
+      this.fileName,
+      JSON.stringify(content, null, 2)
+    );
   }
 }
+
+//CREAR ARCHIVO
+const file = new Contenedor("productos.txt");
+
+//AGREGAR REGISTROS
+// const addEntry = async (obj) => {
+//   console.log(await file.save(obj));
+// };
+// addEntry({
+//   title: "ELDEN RING",
+//   price: "60.00",
+//   thumbnal:
+//     "https://cdn.akamai.steamstatic.com/steam/apps/1245620/header.jpg?t=1683618443",
+// // });
+// addEntry({
+//   title: "DEATH STRANDING",
+//   price: "30.00",
+//   thumbnal:
+//     "https://cdn.akamai.steamstatic.com/steam/apps/1850570/header.jpg?t=1683196693",
+// });
+// addEntry({
+//   title: "DEVIL MAY CRY 5",
+//   price: "30.00",
+//   thumbnal:
+//     "https://cdn.akamai.steamstatic.com/steam/apps/601150/header.jpg?t=1675151951",
+// });
+
+//TRAER TODOS LOS ELEMENTOS
+// const getElements = async () => {
+//   console.log(await file.getAll());
+// };
+// getElements();
+
+//BUSCAR POR ID
+// const getElement = async (id) => {
+//   console.log(await file.getById(id));
+// };
+// getElement(1);
+
+// //ELIMINAR POR ID
+// const removeElement = async (id) => {
+//   await file.deleteById(id);
+// };
+// removeElement(2);
+
+// //ELIMINAR TODOS
+// const removeElements = async () => {
+//   await file.deleteAll();
+// };
+// removeElements();
