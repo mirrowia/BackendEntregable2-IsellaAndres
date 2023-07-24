2e5c938472f4ef3861d8cd5becc7f447cccb7bce
MirrowIA <andresisella@gmail.com>
2023-07-24 07:21:01 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const fs = requiere("fs").promises;
+class Contenedor {
+  constructor(fileName) {
+    this.filename = fileName;
+    this.id = id;
+  }
+
+  async save(Object) {
+    fs.appendFileSync();
+  }
+
+  async getById(id) {}
+
+  async getAll() {}
+  deleteById(id) {}
+  deleteAll() {}
+}
